3a8dfe7c8ec9376fd879c47a8abf4854687e46c1
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-02-20 10:41:56 +0200

    Use init in record and not set

--- DatabaseApiCode/Models/UniversitySpendingsModel.cs
+++ DatabaseApiCode/Models/UniversitySpendingsModel.cs
@@ -6,8 +6,8 @@ public record UniversitySpendingsModel
 {
     public int AllocationYear { get; init; }
     public int UniversityID { get; init; }
-    public decimal TotalAmount { get; set; }
-    public decimal AmountRemaining { get; set; }
-    public List<StudentInfoModel> FundedStudents { get; set; }
+    public decimal TotalAmount { get; init; }
+    public decimal AmountRemaining { get; init; }
+    public List<StudentInfoModel> FundedStudents { get; init; }
 
 }
